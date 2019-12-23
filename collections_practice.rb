d4ceaa53737ac95ffb3625c066c4992dd48ca2ba
Maynard Hartman <mjhartman@brighthouse.com>
2019-12-23 19:40:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+require 'pry'
+#code 
+def sort_array_asc(target)
+  return(target.sort { |a, b| a <=> b }) #sort ascending
+  binding.pry
+end
 
+def sort_array_desc(target) 
+  return(target.sort { |a, b| b <=> a }) #sort descending
+end
+
+def sort_array_char_count(target)
+  
